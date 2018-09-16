f360097485ed61c2a2c0745de22c93695092a954
YunLambert <469066072@qq.com>
2018-09-16 21:31:20 +0800

    内核空间的线程

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,6 +1,10 @@
 #include "print.h"
 #include "init.h"
 #include "memory.h"
+#include "../thread/thread.h"
+
+void k_thread_a(void*);
+
 int main(void) {
    put_str("This is YunOS System,Checking...\n");
    init_all();
@@ -10,6 +14,17 @@ int main(void) {
    put_int((uint32_t)addr);
    put_str("\n");
 
+   thread_start("k_thread_a", 31, k_thread_a, "argA ");
+
    while(1);
    return 0;
+}
+
+/* 在线程中运行的函数 */
+void k_thread_a(void* arg) {     
+/* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
+   char* para = arg;
+   while(1) {
+      put_str(para);
+   }
 }
