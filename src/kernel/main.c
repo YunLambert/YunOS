fee7046e603802ca94c51e12c9c6ba79cd9d818d
YunLambert <469066072@qq.com>
2018-09-17 00:11:39 +0800

    抢夺式线程调度

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,8 +2,11 @@
 #include "init.h"
 #include "memory.h"
 #include "../thread/thread.h"
+#include "interrupt.h"
+#include "../device/console.h"
 
 void k_thread_a(void*);
+void k_thread_b(void*);
 
 int main(void) {
    put_str("This is YunOS System,Checking...\n");
@@ -15,8 +18,12 @@ int main(void) {
    put_str("\n");
 
    thread_start("k_thread_a", 31, k_thread_a, "argA ");
+   thread_start("k_thread_b", 8, k_thread_b, "argB ");
 
-   while(1);
+   intr_enable();	// 打开中断,使时钟中断起作用
+   while(1) {
+       console_put_str("Main ");
+   };
    return 0;
 }
 
@@ -25,6 +32,15 @@ void k_thread_a(void* arg) {
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
    char* para = arg;
    while(1) {
-      put_str(para);
+      console_put_str(para);
+   }
+}
+
+/* 在线程中运行的函数 */
+void k_thread_b(void* arg) {     
+/* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
+   char* para = arg;
+   while(1) {
+       console_put_str(para);
    }
 }
